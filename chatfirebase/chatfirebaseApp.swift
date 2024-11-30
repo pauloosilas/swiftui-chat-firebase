48247f7aca2f68d016b2e0c234f3aec118749fb0
pauloosilas <paulo3356@gmail.com>
2024-11-30 19:17:52 -0300

    Initial Commit

---
+++ chatfirebase/chatfirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatfirebaseApp.swift
+//  chatfirebase
+//
+//  Created by paulo on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct chatfirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
